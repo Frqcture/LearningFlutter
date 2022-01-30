5eb4d0bf7eb670ef91d4ae2afe789174c2a41236
Frqcture <joedavidmoore.jm@gmail.com>
2022-01-30 18:41:06 +0000

    Fixing Overflow, just need to format properly

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,16 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     PostData data = PostData();
-    
+
     StartUp mainPage = StartUp();
-    return mainPage.main(data.getNames(), data.getImages(), data.getCommentsUser(), data.getCommentsMessage());
+    return mainPage.main(data.getNames(), data.getImages(),
+        data.getCommentsUser(), data.getCommentsMessage());
   }
 }
 
 class StartUp {
-  Widget main(List _names, List _images,List CommentUser, List CommentMessage) {
+  Widget main(
+      List _names, List _images, List CommentUser, List CommentMessage) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -45,20 +47,18 @@ class StartUp {
                                     child: Text(e),
                                     alignment: Alignment.centerLeft),
                               ),
-                              Image.asset(_images[e.indexOf(e)],width: 375, height: 325, fit: BoxFit.fitHeight),
+                              Image.asset(_images[e.indexOf(e)],
+                                  width: 375,
+                                  height: 325,
+                                  fit: BoxFit.fitHeight),
                               Padding(
                                 padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                                // child: Align(
-                                //   child: Text('Test data'),
-                                //   alignment: Alignment.centerLeft,
-                                // ),
                                 child: Row(
                                   children: [
-                                    Text(CommentUser[0],),
-                                    Text(': '),
-                                    Text(CommentMessage[0],softWrap: true,)
-                                  ],
-                                ),
+                                    Text(CommentUser[0] + ': ', style:  TextStyle(fontWeight: FontWeight.bold),),
+                                    Expanded(
+                                      child: Text(CommentMessage[0], maxLines: 2, overflow: TextOverflow.ellipsis,) )
+                                ],),
                               ),
                             ],
                           ),
@@ -70,7 +70,8 @@ class StartUp {
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-            BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Profile')
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_box), label: 'Profile')
           ],
         )),
         backgroundColor: Colors.deepPurple,
@@ -81,40 +82,42 @@ class StartUp {
 
 class PostData {
   final List<String> _names = [
-      'Frqcture',
-      'JoeMama',
-      'more test data',
-      'i want to see if this scrolls'
-    ];
+    'Frqcture',
+    'JoeMama',
+    'more test data',
+    'i want to see if this scrolls'
+  ];
 
-    final List<String> _images = [
-      'assets/image.jpg.webp',
-      'assets/image.jpg.webp',
-      'assets/image.jpg.webp',
-      'assets/image.jpg.webp'
-    ];
+  final List<String> _images = [
+    'assets/image.jpg.webp',
+    'assets/image.jpg.webp',
+    'assets/image.jpg.webp',
+    'assets/image.jpg.webp'
+  ];
 
-    final List<String> _commentsUser = ['Frqcture'];
-    final List<String> _commentsMessage = ['This is a test to see if the comments work, what if i make them even longer so they need to go onto the next line or two'];
+  final List<String> _commentsUser = ['Frqcture'];
+  final List<String> _commentsMessage = [
+    'This is a test to see if the comments work, what if i make them even longer so they need to go onto the next line or two'
+  ];
 
-    List getNames() {
-      return _names;
-    }
+  List getNames() {
+    return _names;
+  }
 
-    List getImages() {
-      return _images;
-    }
+  List getImages() {
+    return _images;
+  }
 
-    List getCommentsUser() {
-      return _commentsUser;
-    }
+  List getCommentsUser() {
+    return _commentsUser;
+  }
 
-    List getCommentsMessage() {
-      return _commentsMessage;
-    }
+  List getCommentsMessage() {
+    return _commentsMessage;
+  }
 
-    void addImage(String UserName,String Message) {
-      _commentsUser.add(UserName);
-      _commentsMessage.add(Message);
-    }
+  void addImage(String UserName, String Message) {
+    _commentsUser.add(UserName);
+    _commentsMessage.add(Message);
+  }
 }
